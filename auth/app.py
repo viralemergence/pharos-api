b991383110217f538980331a693748c70a9af059
Ryan <rzimmerman@talusanalytics.com>
2022-06-29 15:44:53 -0300

    aws sam init project with simple auth function as demo

---
+++ auth/app.py
@@ -0,0 +1,52 @@
+import json
+
+# import requests
+
+
+def lambda_handler(event, context):
+
+    """Sample pure Lambda function
+
+    Parameters
+    ----------
+    event: dict, required
+        API Gateway Lambda Proxy Input Format
+
+        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+
+    context: object, required
+        Lambda Context runtime methods and attributes
+
+        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+
+    Returns
+    ------
+    API Gateway Lambda Proxy Output Format: dict
+
+        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+    """
+
+    post_data = json.loads(event.get("body", "{}"))
+
+    # Fake auth response for valid user
+    if post_data["id"] == "1234":
+        return {
+            "statusCode": 200,
+            "body": json.dumps(
+                {
+                    "token": "12-309oij13120-8254",
+                    "username": "dev user",
+                    "email": "devs@talusanalytics.com",
+                    "userID": "5431"
+                }
+            ),
+        }
+
+    return {
+        "statusCode": 401,
+        "body": json.dumps(
+            {
+                "message": "User does not exist",
+            }
+        ),
+    }
