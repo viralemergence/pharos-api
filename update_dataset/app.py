5474b0d15c3957a0b421ca4c5d0fdf95fd154d4f
David Rosado <drosado@talusanalytics.com>
2022-07-05 10:17:28 -0400

    update-dataset working

--- update_dataset/app.py
+++ update_dataset/app.py
@@ -9,12 +9,11 @@ CORS_ALLOW = os.environ["CORS_ALLOW"]
 USERS_TABLE = DYNAMODB.Table(os.environ["USERS_TABLE_NAME"])
 DATASETS_TABLE = DYNAMODB.Table(os.environ["DATASETS_TABLE_NAME"])
 
-
-def lambda_handler(event, context):
-
+def lambda_handler(event, context) -> "POST":
+    
     post_data = json.loads(event.get("body", "{}"))
 
-    # Verify researcher id is in USERS_TABLE
+     # Verify researcher id is in USERS_TABLE
     try:
         users_response = USERS_TABLE.get_item(
             Key={"researcherID": post_data["researcherID"]}
@@ -38,43 +37,35 @@ def lambda_handler(event, context):
             },
             "body": json.dumps({"message": str(e)}),
         } # This should be logged
-   
-    # TODO: Integrate with S3 bucket 
-    s3location = "s3://something"
+
+    # TODO: This section needs to be integrated
+    s3location = "s3://something" # TODO
+    dataset = {
+        "uri" : "s3location",
+        "date" : str( datetime.utcnow() )
+    }
 
     try:
-        date = datetime.utcnow() # Date should be standardized to UTC
-        datasetid = int(datetime.timestamp(date)) # Create a unique timestamp for dataset id. Could be repeated for different researchers
-
-        response = DATASETS_TABLE.put_item(
-            Item = {
-                "researcherID" : post_data["researcherID"],
-                "datasetID" : datasetid,
-                "name" : post_data["dataset_name"],
-                "samples_taken" : post_data["samples_taken"],
-                "detection_run" : post_data["detection_run"],
-                "versions" : [
-                    {
-                        "uri" : s3location,
-                        "date" : str(date) # DyanamoDb does not support date types
-                    }
-                ]
-            }
+        response = DATASETS_TABLE.update_item(
+            Key = {"researcherID" : post_data["researcherID"], "datasetID": post_data["datasetID"]},
+            UpdateExpression = "SET versions = list_append(versions,:d)",
+            ExpressionAttributeValues={":d" : [dataset]},
+            ReturnValues="UPDATED_NEW"
         )
-    
+
     except Exception as e:
         return {
-            "statusCode": 403,
+            "statusCode": 500,
             "headers": {
                 "Access-Control-Allow-Origin": CORS_ALLOW,
             },
             "body": json.dumps({"message": str(e)}),
         } # This should be logged
 
-    return {
+    return { # Change essage to the user
             "statusCode": 200,
             "headers": {
                 "Access-Control-Allow-Origin": CORS_ALLOW,
             },
-            "body": json.dumps({"datasetID": datasetid}),
+            "body": json.dumps({"dataset": dataset}),
         }
