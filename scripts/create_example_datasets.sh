ff6941c229b01367974e75f241496b67a64331bc
David Rosado <drosado@talusanalytics.com>
2022-07-06 13:44:17 -0400

    testing creating dataset functionality

---
+++ scripts/create_example_datasets.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+api='http://localhost:3000'
+
+
+while [[ "$#" > 0 ]];
+  do case $1 in
+    -a|--api) api=$2; shift;;
+    *) echo "Unknown parameter passed: $1"; exit 1 ;;
+  esac
+  shift
+done
+
+
+curl -d \
+  '{"researcherID" : "12345", "dataset_name" : "zoo", "samples_taken" : 1, "detection_run" : 0}'\
+  -X POST $api/create-dataset;
