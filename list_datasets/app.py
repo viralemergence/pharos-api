8d2cb881814ccaef86332829fe7b70af47372a0f
Ryan <rzimmerman@talusanalytics.com>
2022-07-06 12:23:05 -0300

    Black python formatting

--- list_datasets/app.py
+++ list_datasets/app.py
@@ -10,22 +10,26 @@ CORS_ALLOW = os.environ["CORS_ALLOW"]
 USERS_TABLE = DYNAMODB.Table(os.environ["USERS_TABLE_NAME"])
 DATASETS_TABLE = DYNAMODB.Table(os.environ["DATASETS_TABLE_NAME"])
 
+
 class DecimalEncoder(json.JSONEncoder):
-  def default(self, obj):
-    if isinstance(obj, Decimal):
-      return str(obj)
-    return json.JSONEncoder.default(self, obj)
+    def default(self, obj):
+        if isinstance(obj, Decimal):
+            return str(obj)
+        return json.JSONEncoder.default(self, obj)
+
 
 def lambda_handler(event, context):
 
     post_data = json.loads(event.get("body", "{}"))
-    
+
     try:
         # This might have to change because it only queries by page (?).
         response = DATASETS_TABLE.query(
-            KeyConditionExpression=Key("researcherID").eq(post_data["researcherID"]) # Query only by partition key
+            KeyConditionExpression=Key("researcherID").eq(
+                post_data["researcherID"]
+            )  # Query only by partition key
         )
-    
+
     except Exception as e:
         return {
             "statusCode": 500,
@@ -33,12 +37,14 @@ def lambda_handler(event, context):
                 "Access-Control-Allow-Origin": CORS_ALLOW,
             },
             "body": json.dumps({"message": str(e)}),
-        } # This should be logged
+        }  # This should be logged
 
     return {
-            "statusCode": 200,
-            "headers": {
-                "Access-Control-Allow-Origin": CORS_ALLOW,
-            },
-            "body": json.dumps({"datasets": response["Items"]}, cls=DecimalEncoder), # Returns a dictionary with a list of datasets
-        }
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": CORS_ALLOW,
+        },
+        "body": json.dumps(
+            {"datasets": response["Items"]}, cls=DecimalEncoder
+        ),  # Returns a dictionary with a list of datasets
+    }
--- update_dataset/app.py
+++ update_dataset/app.py
@@ -9,11 +9,12 @@ CORS_ALLOW = os.environ["CORS_ALLOW"]
 USERS_TABLE = DYNAMODB.Table(os.environ["USERS_TABLE_NAME"])
 DATASETS_TABLE = DYNAMODB.Table(os.environ["DATASETS_TABLE_NAME"])
 
+
 def lambda_handler(event, context) -> "POST":
-    
+
     post_data = json.loads(event.get("body", "{}"))
 
-     # Verify researcher id is in USERS_TABLE
+    # Verify researcher id is in USERS_TABLE
     try:
         users_response = USERS_TABLE.get_item(
             Key={"researcherID": post_data["researcherID"]}
@@ -21,7 +22,7 @@ def lambda_handler(event, context) -> "POST":
 
         # Exit if user is not in the database.
         if "Item" not in users_response:
-           return {
+            return {
                 "statusCode": 403,
                 "headers": {
                     "Access-Control-Allow-Origin": CORS_ALLOW,
@@ -36,21 +37,21 @@ def lambda_handler(event, context) -> "POST":
                 "Access-Control-Allow-Origin": CORS_ALLOW,
             },
             "body": json.dumps({"message": str(e)}),
-        } # This should be logged
+        }  # This should be logged
 
     # TODO: This section needs to be integrated
-    s3location = "s3://something" # TODO
-    dataset = {
-        "uri" : "s3location",
-        "date" : str( datetime.utcnow() )
-    }
+    s3location = "s3://something"  # TODO
+    dataset = {"uri": "s3location", "date": str(datetime.utcnow())}
 
     try:
         response = DATASETS_TABLE.update_item(
-            Key = {"researcherID" : post_data["researcherID"], "datasetID": post_data["datasetID"]},
-            UpdateExpression = "SET versions = list_append(versions,:d)",
-            ExpressionAttributeValues={":d" : [dataset]},
-            ReturnValues="UPDATED_NEW"
+            Key={
+                "researcherID": post_data["researcherID"],
+                "datasetID": post_data["datasetID"],
+            },
+            UpdateExpression="SET versions = list_append(versions,:d)",
+            ExpressionAttributeValues={":d": [dataset]},
+            ReturnValues="UPDATED_NEW",
         )
 
     except Exception as e:
@@ -60,12 +61,12 @@ def lambda_handler(event, context) -> "POST":
                 "Access-Control-Allow-Origin": CORS_ALLOW,
             },
             "body": json.dumps({"message": str(e)}),
-        } # This should be logged
+        }  # This should be logged
 
-    return { # Change essage to the user
-            "statusCode": 200,
-            "headers": {
-                "Access-Control-Allow-Origin": CORS_ALLOW,
-            },
-            "body": json.dumps({"dataset": dataset}),
-        }
+    return {  # Change essage to the user
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": CORS_ALLOW,
+        },
+        "body": json.dumps({"dataset": dataset}),
+    }
