ff334b5bdb6af20c1c03bd886c6f4d59e0c054cb
David Rosado <drosado@talusanalytics.com>
2022-07-07 17:45:34 -0400

    read_version working

---
+++ read_version/app.py
@@ -0,0 +1,31 @@
+import boto3
+import json
+import os
+
+S3CLIENT = boto3.client("s3")
+DATASETS_S3_BUCKET = os.environ["DATASETS_S3_BUCKET"]
+CORS_ALLOW = os.environ["CORS_ALLOW"]
+
+
+def lambda_handler(event, context):
+
+    post_data = post_data = json.loads(event.get("body", "{}"))
+
+    try:
+        response = S3CLIENT.get_object(Bucket=DATASETS_S3_BUCKET, Key=post_data["key"])
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "headers": {
+                "Access-Control-Allow-Origin": CORS_ALLOW,
+            },
+            "body": json.dumps({"message": str(e)}),
+        }  # This
+
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": CORS_ALLOW,
+        },
+        "body": json.dumps({"response": response["Body"].read().decode("UTF-8")}),
+    }
