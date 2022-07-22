1a926c672344c229e992182948f60eaebde961e2
Ryan <rzimmerman@talusanalytics.com>
2022-07-22 18:06:19 -0300

    Fixing hashing

--- save_register/app.py
+++ save_register/app.py
@@ -14,7 +14,7 @@ USERS_TABLE = DYNAMODB.Table(os.environ["USERS_TABLE_NAME"])
 DATASETS_TABLE = DYNAMODB.Table(os.environ["DATASETS_TABLE_NAME"])
 
 
-def lambda_handler(event):
+def lambda_handler(event, _):
 
     post_data = json.loads(event.get("body", "{}"))
 
@@ -46,14 +46,19 @@ def lambda_handler(event):
     # Storing data to S3 BUCKET
     try:
 
-        md5hash = str(hashlib.md5(post_data["rows"].encode("utf-8").hexdigest()))
+        # in a variable since we'll use it twice
+        data_string = bytes(json.dumps(post_data["data"]).encode("UTF-8"))
+
+        md5hash = str(hashlib.md5(data_string).hexdigest())
         key = f'{post_data["registerID"]}/{md5hash}.json'
 
         response = S3CLIENT.put_object(  # pylint: disable=unused-variable
             Bucket=DATASETS_S3_BUCKET,
             Key=key,
-            Body=(bytes(json.dumps(post_data["rows"]).encode("UTF-8"))),
+            Body=(data_string),
         )
+
+        print(response)
 
     except Exception as e:  # pylint: disable=broad-except
         return {
