84af7d9fcd30f62379687d89257a746d28dce718
Ryan <rzimmerman@talusanalytics.com>
2022-07-28 14:30:26 -0300

    Refactoring and cleaning a little bit, changing response codes

--- save_register/app.py
+++ save_register/app.py
@@ -15,65 +15,44 @@ USERS_TABLE = DYNAMODB.Table(os.environ["USERS_TABLE_NAME"])
 DATASETS_TABLE = DYNAMODB.Table(os.environ["DATASETS_TABLE_NAME"])
 
 
-def error_response(e):
+def format_response(code, body):
     return {
-        "statusCode": 403,
+        "statusCode": code,
         "headers": {"Access-Control-Allow-Origin": CORS_ALLOW},
-        "body": json.dumps({"exception": str(e)}),
+        "body": json.dumps(body),
     }
 
 
-def lambda_handler(event, _):
-
-    post_data = json.loads(event.get("body", "{}"))
-
-    # # Verify researcher id is in USERS_TABLE - Authentication FAKE
+def check_auth(researcherID):
+    # Verify researcher id is in USERS_TABLE - Authentication FAKE
     try:
-        users_response = USERS_TABLE.get_item(
-            Key={"researcherID": post_data["researcherID"]}
-        )
-
+        users_response = USERS_TABLE.get_item(Key={"researcherID": researcherID})
         # Exit if user is not in the database.
         if "Item" not in users_response:
-            return {
-                "statusCode": 403,
-                "headers": {"Access-Control-Allow-Origin": CORS_ALLOW},
-                "body": json.dumps({"message": "User does not exist"}),
-            }
+            return False
 
-    except Exception as e:  # pylint: disable=broad-except
-        return {
-            "statusCode": 500,
-            "headers": {"Access-Control-Allow-Origin": CORS_ALLOW},
-            "body": json.dumps({"message": str(e)}),
-        }  # This should be logged
+    except Exception:  # pylint: disable=broad-except
+        return False
 
-    # Storing data to S3 BUCKET
+    return True
+
+
+def lambda_handler(event, _):
+    post_data = json.loads(event.get("body", "{}"))
+
+    # Placeholder check user authorization
+    authorized = check_auth(post_data["researcherID"])
+    if not authorized:
+        return format_response(403, "Not Authorized")
+
     try:
-
-        # in a variable since we'll use it twice
-        data_string = bytes(json.dumps(post_data["data"]).encode("UTF-8"))
-
-        md5hash = str(hashlib.md5(data_string).hexdigest())
+        # Create a unique key by combining the datasetID and the register hash
+        encoded_data = bytes(json.dumps(post_data["data"]).encode("UTF-8"))
+        md5hash = str(hashlib.md5(encoded_data).hexdigest())
         key = f'{post_data["datasetID"]}/{md5hash}.json'
 
-        save = S3CLIENT.put_object(
-            Bucket=DATASETS_S3_BUCKET,
-            Body=(data_string),
-            Key=key,
-        )
-
-    except Exception as e:  # pylint: disable=broad-except
-        return error_response(e)
-
-    try:
-        # Verify the new register has been saved
-        S3CLIENT.head_object(Bucket=DATASETS_S3_BUCKET, Key=key)
-
-    except Exception as e:  # pylint: disable=broad-except
-        return error_response(e)
-
-    try:
+        # Save new register object to S3 bucket
+        S3CLIENT.put_object(Bucket=DATASETS_S3_BUCKET, Body=(encoded_data), Key=key)
 
         # Check the number of files inside the folder
         dataset_list = S3CLIENT.list_objects_v2(
@@ -86,47 +65,9 @@ def lambda_handler(event, _):
         if length > int(N_VERSIONS):
             dataset_list["Contents"].sort(key=lambda item: item["LastModified"])
             delkey = dataset_list["Contents"][0]["Key"]
-            response = S3CLIENT.delete_object(Bucket=DATASETS_S3_BUCKET, Key=delkey)
+            S3CLIENT.delete_object(Bucket=DATASETS_S3_BUCKET, Key=delkey)
 
-        return {
-            # sending the status code on so that the
-            # response actually reflects the success of saving
-            "statusCode": save["ResponseMetadata"]["HTTPStatusCode"],
-            "headers": {"Access-Control-Allow-Origin": CORS_ALLOW},
-            "body": json.dumps(post_data["data"]),
-        }
+        return format_response(200, post_data["data"])
 
     except Exception as e:  # pylint: disable=broad-except
-        return error_response(e)
-
-    # dataset = {"key": key, "date": post_data["date"]}
-
-    # # Update version
-    # try:
-    #     response = DATASETS_TABLE.update_item(
-    #         Key={
-    #             "researcherID": post_data["researcherID"],  # Partition Key
-    #             "datasetID": post_data["datasetID"],  # Sort Key
-    #         },
-    #         # Append to version list
-    #         UpdateExpression="SET versions = list_append(versions,:d)",
-    #         ExpressionAttributeValues={":d": [dataset]},
-    #         ReturnValues="UPDATED_NEW",
-    #     )
-
-    # except Exception as e:
-    #     return {
-    #         "statusCode": 500,
-    #         "headers": {
-    #             "Access-Control-Allow-Origin": CORS_ALLOW,
-    #         },
-    #         "body": json.dumps({"message": str(e)}),
-    #     }  # This should be logged
-
-    # return {
-    #     "statusCode": 200,
-    #     "headers": {
-    #         "Access-Control-Allow-Origin": CORS_ALLOW,
-    #     },
-    #     "body": json.dumps({"key": key}),
-    # }
+        return format_response(403, e)
