8f43e420fa9f273800d51dd02f19d69270cc6538
David Rosado <drosado@talusanalytics.com>
2022-08-11 15:46:51 -0400

    project update

--- src/lambda/save_project/app.py
+++ src/lambda/save_project/app.py
@@ -14,7 +14,7 @@ PROJECTS_TABLE = DYNAMODB.Table(os.environ["PROJECTS_TABLE_NAME"])
 def lambda_handler(event, _):
     post_data = json.loads(event.get("body", "{}"))
 
-    authorized = check_auth(post_data["researcherID"])
+    authorized = check_auth(post_data["authors"][0]["researcherID"])
     if not authorized:
         return format_response(403, "Not Authorized")
 
@@ -27,12 +27,11 @@ def lambda_handler(event, _):
             USERS_TABLE.update_item(
                 Key={"researcherID": researcher},
                 # Dynamodb docs specify ADD for sets
-                UpdateExpression="ADD projects = :i)",
+                UpdateExpression="ADD projects :i",
                 # Need to indicate it is a string set - SS
-                ExpressionAttributeValues={":i": {"SS": post_data["projectID"]}},
-                ReturnValues="UPDATED_NEW",
+                ExpressionAttributeValues={":i": set([post_data["projectID"]])},
             )
         return format_response(200, "")
 
     except Exception as e:  # pylint: disable=broad-except
-        return format_response(403, e)
+        return format_response(403, str(e))
