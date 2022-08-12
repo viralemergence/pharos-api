9431bc24becb34914468f3781901cc1a666fbffa
David Rosado <drosado@talusanalytics.com>
2022-08-12 12:24:12 -0400

    project batch_get_item refactor

--- src/lambda/list_projects/app.py
+++ src/lambda/list_projects/app.py
@@ -1,11 +1,12 @@
 import json
 import os
-
 import boto3
+from boto3.dynamodb.conditions import Key
 from auth import check_auth
 from format import format_response
 
 DYNAMODB = boto3.resource("dynamodb")
+USERS_TABLE = DYNAMODB.Table(os.environ["USERS_TABLE_NAME"])
 PROJECTS_TABLE = os.environ["PROJECTS_TABLE_NAME"]
 
 
@@ -17,14 +18,16 @@ def lambda_handler(event, _):
         return format_response(403, "Not Authorized")
 
     try:
+
+        user = USERS_TABLE.get_item(Key={"researcherID": post_data["researcherID"]})
+        projectids = user["Item"]["projectIDs"]
+
         projects = DYNAMODB.batch_get_item(
             RequestItems={
-                PROJECTS_TABLE: {
-                    "Keys": [{"projectID": {"S": pID}} for pID in post_data["projects"]]
-                }
+                PROJECTS_TABLE: {"Keys": [{"projectID": pID} for pID in projectids]}
             }
         )
-        return format_response(200, projects)
+        return format_response(200, projects["Responses"][PROJECTS_TABLE])
 
     except Exception as e:  # pylint: disable=broad-except
-        return format_response(403, e)
+        return format_response(403, str(e))
