d2d97407aba6aba451ae7a901182174d7c1db4a2
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2022-08-12 12:46:12 -0300

    Renaming "projects" array to "projectIDs" since it's just the IDs

--- src/lambda/list_projects/app.py
+++ src/lambda/list_projects/app.py
@@ -17,14 +17,27 @@ def lambda_handler(event, _):
         return format_response(403, "Not Authorized")
 
     try:
+        print(
+            {
+                "Keys": [
+                    {"projectID": {"S": projectID}}
+                    for projectID in post_data["projectIDs"]
+                ]
+            }
+        )
+
         projects = DYNAMODB.batch_get_item(
             RequestItems={
                 PROJECTS_TABLE: {
-                    "Keys": [{"projectID": {"S": pID}} for pID in post_data["projects"]]
+                    "Keys": [
+                        {"projectID": {"S": projectID}}
+                        for projectID in post_data["projectIDs"]
+                    ]
                 }
             }
         )
         return format_response(200, projects)
 
     except Exception as e:  # pylint: disable=broad-except
+        print(e)
         return format_response(403, e)
