639f03e3ccf4e65131c659856e4f0d8998dd067f
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2022-08-12 09:30:51 -0300

    Adding custom encoder for transforming sets into lists before sending

--- src/libraries/python/format.py
+++ src/libraries/python/format.py
@@ -3,10 +3,17 @@ import os
 
 CORS_ALLOW = os.environ["CORS_ALLOW"]
 
+class SetEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, set):
+            return list(o)
+        return json.JSONEncoder.default(self, o)
+
+
 
 def format_response(code, body):
     return {
         "statusCode": code,
         "headers": {"Access-Control-Allow-Origin": CORS_ALLOW},
-        "body": json.dumps(body),
+        "body": json.dumps(body, cls=SetEncoder),
     }
