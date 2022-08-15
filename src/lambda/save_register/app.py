b9a2c192c6bd42c08d823dda876d6b04dd1a6d55
David Rosado <drosado@talusanalytics.com>
2022-08-15 09:53:28 -0400

    bin split for batch upload

--- src/lambda/save_register/app.py
+++ src/lambda/save_register/app.py
@@ -11,6 +11,12 @@ DYNAMODB = boto3.resource("dynamodb")
 DATASETS_TABLE = DYNAMODB.Table(os.environ["DATASETS_TABLE_NAME"])
 
 
+def split(register: list, bin_size=25) -> list:
+    register_length = len(register)
+    for i in range(register_length):
+        yield register[i:bin_size]
+
+
 def lambda_handler(event, _):
     post_data = json.loads(event.get("body", "{}"))
 
@@ -23,12 +29,17 @@ def lambda_handler(event, _):
 
         register = post_data["register"]
 
+        # Need to coerce register to list
+        register_list = list(register.values())
+
         # When code exits with block, batch writer will send the data to DynamoDB.
         # Only allows 25 put_items
+        bins = split(register_list)
 
-        with DATASETS_TABLE.batch_write() as batch:
-            for record in register:
-                batch.put_item(Item=record)
+        for bin_ in bins:
+            with DATASETS_TABLE.batch_write() as batch:
+                for record in bin_:
+                    batch.put_item(Item=record)
 
         return format_response(200, post_data["data"])
 
