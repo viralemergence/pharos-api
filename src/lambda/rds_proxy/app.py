a48f5e4d50bd5af883a0293fe0efb026466d80b2
David Rosado <drosado@talusanalytics.com>
2022-08-30 13:03:21 -0400

    cleaning comments

--- src/lambda/rds_proxy/app.py
+++ src/lambda/rds_proxy/app.py
@@ -12,62 +12,19 @@ from tables import TestTable, AnimalTable
 
 DYNAMODB = boto3.resource("dynamodb")
 USERS_TABLE = DYNAMODB.Table(os.environ["USERS_TABLE_NAME"])
-RDS = boto3.client("rds")
-
-
-# class DbSession:
-#     def __init__(self, engine):
-#         self.engine = engine
-
-#     def __enter__(self):
-#         self.connection = (
-#             self.engine.connect()
-#         )  # // Reuse the engine instead of creating a new one
-#         self.session = scoped_session(
-#             sessionmaker(autocommit=False, autoflush=False, bind=self.engine)
-#         )
-#         return self.session
-
-#     def __exit__(self, type, value, traceback):
-#         self.session.commit()
-#         self.session.close()
-#         self.connection.close()
-
-
-# This function might need to be async as per AWS documentation:
-# You must ensure that you have static or previously resolved credentials if you call this method synchronously (with no callback),
-# # otherwise it may not properly sign the request. If you cannot guarantee this (you are using an asynchronous credential provider,
-# # i.e., EC2 IAM roles), you should always call this method with an asynchronous callback.
-
-# IAM authentication is required, hence IAM token must be created
-# def proxy_token(parameters):
-#     token = RDS.generate_db_auth_token(
-#         DBHostname=parameters["host"],  # RDS proxy endpoint
-#         Port=parameters["port"],  # RDS proxy endpoint
-#         DBUsername=parameters["username"],  # DB schema user
-#     )
-#     return token
-
-# Set secretmanager in the meantime.
 
 
 def database(parameters):
-    # token = proxy_token(parameters) - TODO
     database_url = f"postgresql://{parameters['username']}:{parameters['password']}@{parameters['host']}:{parameters['port']}/{parameters['database']}?sslmode=require"
     engine = sqlalchemy.create_engine(database_url)
     return engine
 
 
-def unpack():
-    pass
-
-
 def lambda_handler(event, _):
+    # Minimal working example
     post_data = json.loads(event.get("body", "{}"))
 
     today = datetime.date.today()
-    # d1 = today.strftime("%d/%m/%Y")
-
     try:
         engine = database(post_data["parameters"])
 
@@ -102,27 +59,3 @@ def lambda_handler(event, _):
 
     except Exception as e:
         return format_response(403, e)
-
-
-# def lambda_handler(event, _):
-
-#     post_data = json.loads(event.get("body", "{}"))
-#     # Authentication doesn't work, possibly because dynabodb tables are not in same VPC and VPC security group - TODO
-
-#     try:
-#         engine = database(post_data["parameters"])
-
-#         with engine.connect() as connection:
-#             # Something along the lines of...
-
-#             for row in post_data["data"]:
-#                 test, animal = unpack()
-#                 test_query = insert()
-#                 animal_query = insert()
-#                 test_result = connection.execute(test_query)
-#                 animal_result = connection.execute(animal_query)
-
-#         return format_response(200, connection)
-
-#     except Exception as e:
-#         return format_response(403, e)
