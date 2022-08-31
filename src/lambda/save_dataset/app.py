7b552f8400bfb3128587c33ba3a102534e25ccf6
David Rosado <drosado@talusanalytics.com>
2022-08-31 10:56:46 -0400

    removing project table update

--- src/lambda/save_dataset/app.py
+++ src/lambda/save_dataset/app.py
@@ -19,13 +19,13 @@ def lambda_handler(event, _):
 
     try:
         # Append to set of datasetIDs
-        PROJECTS_TABLE.update_item(
-            Key={"projectID": post_data["projectID"]},
-            # Dynamodb docs specify ADD for sets
-            UpdateExpression="ADD datasetIDs :i",
-            # Need to indicate it is a string set - SS
-            ExpressionAttributeValues={":i": set([post_data["datasetID"]])},
-        )
+        # PROJECTS_TABLE.update_item(
+        #     Key={"projectID": post_data["projectID"]},
+        #     # Dynamodb docs specify ADD for sets
+        #     UpdateExpression="ADD datasetIDs :i",
+        #     # Need to indicate it is a string set - SS
+        #     ExpressionAttributeValues={":i": set([post_data["datasetID"]])},
+        # )
 
         # Add dataset
         record = post_data.copy()
