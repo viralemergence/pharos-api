3736c1caa543e196c0073d245399d608e524bb5e
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2022-09-26 16:30:02 -0400

    Updating create_example_users script to keep IDs persistent across deployments
    Running this will make new accounts but with the IDs we already
    sent to everyone.

--- scripts/create_example_users.sh
+++ scripts/create_example_users.sh
@@ -2,8 +2,6 @@
 
 api='http://localhost:3000'
 
-# review: https://wa769evyjf.execute-api.us-east-2.amazonaws.com/Prod
-
 # parse params
 while [[ "$#" > 0 ]];
   do case $1 in
@@ -15,31 +13,27 @@ done
 
 
 curl -d \
-  '{"name":"Ryan Zimmerman", "email":"rzimmerman@talusanalytics.com", "organization":"Talus Analytics"}'\
+  '{"researcherID":"389b8fe9b92c417e943561e8e9837acc", "name":"Ryan Zimmerman", "email":"rzimmerman@talusanalytics.com", "organization":"Talus Analytics"}'\
   -X POST $api/create-user;
 
 curl -d \
-  '{"name":"David Rosado", "email":"drosado@talusanalytics.com", "organization":"Talus Analytics"}'\
+  '{"researcherID":"f9fbebeba38b4628836e8c27f9ce9918", "name":"David Rosado", "email":"drosado@talusanalytics.com", "organization":"Talus Analytics"}'\
   -X POST $api/create-user;
 
 curl -d \
-  '{"name":"Tess Stevens", "email":"tstevens@talusanalytics.com", "organization":"Talus Analytics"}'\
+  '{"researcherID":"7ab11e694c7f44ff8e968575d809418e", "name":"Tess Stevens", "email":"tstevens@talusanalytics.com", "organization":"Talus Analytics"}'\
   -X POST $api/create-user;
 
 curl -d \
-  '{"name":"Ellie Graeden", "email":"egraeden@talusanalytics.com", "organization":"Talus Analytics"}'\
+  '{"researcherID":"d012836ee96b4617809c81edc87f4219", "name":"Ellie Graeden", "email":"egraeden@talusanalytics.com", "organization":"Talus Analytics"}'\
   -X POST $api/create-user;
 
 curl -d \
-  '{"name":"Hailey", "email":"hrobertson@talusanalytics.com", "organization":"Talus Analytics"}'\
+  '{"researcherID":"a2d0efb40799454a8fbb254c627d2ac9", "name":"Hailey", "email":"hrobertson@talusanalytics.com", "organization":"Talus Analytics"}'\
   -X POST $api/create-user;
 
 curl -d \
-  '{"name":"Cassiana Robinson", "email":"crobinson@talusanalytics.com", "organization":"Talus Analytics"}'\
-  -X POST $api/create-user;
-
-curl -d \
-  '{"name":"Colin Carlson", "email":"cjc322@georgetown.edu", "organization":"Talus Analytics"}'\
+  '{"researcherID":"ae2b13939b124d56977841df70cedb76", "name":"Colin Carlson", "email":"cjc322@georgetown.edu", "organization":"Talus Analytics"}'\
   -X POST $api/create-user;
 
 
