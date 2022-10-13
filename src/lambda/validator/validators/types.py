846ca1041deb9bd0b2c482ce090c0faa2088f1aa
David Rosado <drosado@talusanalytics.com>
2022-10-13 14:18:10 -0400

    record def expanded and merge as operator overload

--- src/lambda/validator/validators/types.py
+++ src/lambda/validator/validators/types.py
@@ -1,16 +1,3 @@
-class Record:
-    """
-    Record object matching frontend definition.
-
-    { [key: string]: Datapoint }
-
-    """
-
-    def __init__(self, record: dict):
-        datapoints = {key: Datapoint(value) for key, value in record.items()}
-        self.__dict__.update(datapoints)
-
-
 class Datapoint:
     """
     Datapoint object matching frontend definition.
@@ -38,46 +25,65 @@ class Datapoint:
         return hash(self.__dict__["timestamp"])
 
     def __eq__(self, __o: object) -> bool:
-        return isinstance(__o, Datapoint) and self.__dict__ == __o.__dict__
+        return (
+            isinstance(__o, Datapoint)
+            and self.__dict__ == __o.__dict__
+            # and self.__dict__["timestamp"] == __o.timestamp
+        )
 
 
-def unpack_datapoint(datapoint: Datapoint) -> set:
-    unpacked_datapoints = set()
-    while hasattr(datapoint, "previous"):
-        unpacked_datapoints.add(datapoint)
-        datapoint = datapoint.previous
-    return unpacked_datapoints
+class Record:
+    """
+    Record object matching frontend definition.
 
+    { [key: string]: Datapoint }
 
-def order_datapoints(datapoints: list) -> Datapoint:
-    # Sort list from oldest to latest timestamp
-    sorted(datapoints, key=lambda datapoint: datapoint.timestamp, reverse=True)
-    # Extract the latest datapoint
-    oldest = datapoints.pop(0)
-    # Nest datapoints
-    while datapoints:
-        latest = datapoints.pop(0)
-        latest.preview = oldest
-        oldest = latest
-    return oldest
+    """
 
+    def __init__(self, record: dict):
+        datapoints = {key: Datapoint(value) for key, value in record.items()}
+        self.__dict__.update(datapoints)
 
-def merge(client_record: Record, stored_record: Record) -> Record:
-    # Client record
-    client = {
-        key: unpack_datapoint(Datapoint(value)) for key, value in client_record.items()
-    }
-    # Stored record
-    stored = {
-        key: unpack_datapoint(Datapoint(value)) for key, value in stored_record.items()
-    }
-    # Merge two dictionaries with common and shared keys
-    merged_record = {
-        k: client[k] + stored[k]
-        if (k in client and k in stored)
-        else client.get(k) or stored.get(k)
-        for k in sorted(client.keys() | stored.keys())
-    }
-    # Nest datapoints
-    merged_record = {k: order_datapoints(list(v)) for k, v in merged_record.items()}
-    return Record(merged_record)
+    @staticmethod
+    def unpack_datapoint(datapoint: Datapoint) -> set:
+        unpacked_datapoints = set()
+        while hasattr(datapoint, "previous"):
+            unpacked_datapoints.add(datapoint)
+            datapoint = datapoint.previous
+        return unpacked_datapoints
+
+    @staticmethod
+    def order_datapoints(datapoints: list) -> Datapoint:
+        # Sort list from oldest to latest timestamp
+        sorted(datapoints, key=lambda datapoint: datapoint.timestamp, reverse=True)
+        # Extract the latest datapoint
+        oldest = datapoints.pop(0)
+        # Nest datapoints
+        while datapoints:
+            latest = datapoints.pop(0)
+            latest.preview = oldest
+            oldest = latest
+        return oldest
+
+    def __iadd__(self, __record):
+        # Client record
+        client = {
+            key: self.unpack_datapoint(Datapoint(value))
+            for key, value in self.__dict__.items()
+        }
+        # Stored record
+        stored = {
+            key: self.unpack_datapoint(Datapoint(value))
+            for key, value in __record.__dict__.items()
+        }
+        # Merge two dictionaries with common and shared keys
+        merged_record = {
+            k: client[k].union(stored[k])
+            if (k in client and k in stored)
+            else client.get(k) or stored.get(k)
+            for k in sorted(client.keys() | stored.keys())
+        }
+        # Nest datapoints and update the record
+        self.__dict__.update(
+            {k: self.order_datapoints(list(v)) for k, v in merged_record.items()}
+        )
