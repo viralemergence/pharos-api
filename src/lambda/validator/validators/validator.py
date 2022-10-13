da0c0df5d8031a1939e65f4ebae73c3ea9cbf13d
David Rosado <drosado@talusanalytics.com>
2022-10-13 15:52:43 -0400

    adding record and isfloat function

--- src/lambda/validator/validators/validator.py
+++ src/lambda/validator/validators/validator.py
@@ -1,38 +1,48 @@
-import json
+from abc import ABC, abstractmethod
+from .types import Datapoint, Record
 
 
-class Validator:
-    def __init__(self, datapoint: json):
+def isfloat(num: str):
+    try:
+        float(num)
+        return True
+    except ValueError:
+        return False
+
+
+class Validator(ABC):
+    SUCCESS = "Success"
+    FAILURE = "Failure"
+    WARNING = "Warning"
+
+    def __init__(self, datapoint: Datapoint, record: Record):
         self.datapoint = datapoint
-        self.dataValue = self.datapoint["dataValue"]
-        self.SUCCESS = "Success"
-        self.FAILURE = "Failure"
-        self.WARNING = "Warning"
+        self.record = record
+        self.data_value = self.datapoint.dataValue
 
     def validate_presence(self):
-        """Compels the user to enter data in the required field."""
-        if self.dataValue != "":
+        if self.data_value != "":
             return {"status": self.SUCCESS}
         return {
-            "status": self.FAILURE,
+            "status": self.WARNING,
             "message": "Missing value",
         }
 
+    @abstractmethod
     def validate_type(self):
         """Data type."""
-        raise NotImplementedError
 
+    @abstractmethod
     def validate_format(self):
         """Entry requieres a specific format."""
-        raise NotImplementedError
 
+    @abstractmethod
     def validate_restrictions(self):
         """Field has a definite amount of data that can be entered into them."""
-        raise NotImplementedError
 
-    # def validate_referential_integrity(self):
-    #     """Impose referential integrity to validate inputs. Check data inputs in certain fields against values in database tables."""
-    #     raise NotImplementedError
+    @abstractmethod
+    def validate_referential_integrity(self):
+        """Impose referential integrity to validate inputs. Check data inputs in certain fields against values in database tables."""
 
     def run_validation(self):
         if "report" not in self.datapoint:
