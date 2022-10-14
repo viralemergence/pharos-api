fec65ffedd84a75bc479202fb465476a40dce7ff
David Rosado <drosado@talusanalytics.com>
2022-10-14 12:07:59 -0400

    name change

---
+++ src/lambda/validator/validators/type.py
@@ -0,0 +1,91 @@
+class Datapoint:
+    """
+    Datapoint object matching frontend definition.
+
+    displayValue: string
+    dataValue: string | { [key: string]: string }
+    report?: {
+        pass: ReportScore
+        message: string
+        data: { [key: string]: string }
+    }
+    modifiedBy: string
+    version: string
+    previous?: Datapoint
+    """
+
+    report: dict = None
+
+    def __init__(self, datapoint: dict):
+        self.__dict__.update(datapoint)
+        datapoint_ = self
+        if hasattr(datapoint_, "previous"):
+            previous = Datapoint(datapoint_.previous)
+            datapoint_.previous = previous
+
+    def __hash__(self) -> int:
+        return hash(self.__dict__["timestamp"])
+
+    def __eq__(self, __o: object) -> bool:
+        return isinstance(__o, Datapoint) and self.__dict__ == __o.__dict__
+
+
+class Record:
+    """
+    Record object matching frontend definition.
+
+    { [key: string]: Datapoint }
+
+    """
+
+    def __init__(self, record: dict):
+        datapoints = {key: Datapoint(value) for key, value in record.items()}
+        self.__dict__.update(datapoints)
+
+    @staticmethod
+    def unpack_datapoint(datapoint: Datapoint) -> set:
+        unpacked_datapoints = set()
+        while hasattr(datapoint, "previous"):
+            unpacked_datapoints.add(datapoint)
+            datapoint = datapoint.previous
+        return unpacked_datapoints
+
+    @staticmethod
+    def order_datapoints(datapoints: list) -> Datapoint:
+        # Sort list from oldest to latest timestamp
+        sorted(datapoints, key=lambda datapoint: datapoint.timestamp, reverse=True)
+        # Extract the latest datapoint
+        oldest = datapoints.pop(0)
+        # Nest datapoints
+        while datapoints:
+            latest = datapoints.pop(0)
+            latest.preview = oldest
+            oldest = latest
+        return oldest
+
+    def __iadd__(self, __record):
+        """
+        Updates the record by overloading the operator +=.
+        This can be achieved with: record1 += record2
+        """
+        # Client record
+        client = {
+            key: self.unpack_datapoint(Datapoint(value))
+            for key, value in self.__dict__.items()
+        }
+        # Stored record
+        stored = {
+            key: self.unpack_datapoint(Datapoint(value))
+            for key, value in __record.__dict__.items()
+        }
+        # Merge two dictionaries with common and shared keys
+        merged_record = {
+            k: client[k].union(stored[k])
+            if (k in client and k in stored)
+            else client.get(k) or stored.get(k)
+            for k in sorted(client.keys() | stored.keys())
+        }
+        # Nest datapoints and update the record
+        self.__dict__.update(
+            {k: self.order_datapoints(list(v)) for k, v in merged_record.items()}
+        )
