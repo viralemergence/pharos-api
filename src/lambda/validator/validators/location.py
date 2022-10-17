41850b506c8a85be8fc4f3ae33b643590acb4249
David Rosado <drosado@talusanalytics.com>
2022-10-17 12:39:27 -0400

    location part of record

--- src/lambda/validator/validators/location.py
+++ src/lambda/validator/validators/location.py
@@ -2,27 +2,19 @@ from .validator import Validator, isfloat
 
 
 class Location(Validator):
-    def validate_type(self):
-        if isfloat(self.data_value):
+    def __validate_type(self):
+        if isfloat(self.dataValue):
             return {"status": self.SUCCESS}
         return {
             "status": self.FAILURE,
             "message": "Decimal degrees should be expressed with . and not ,",
         }
 
-    def validate_format(self):
-        sequences = self.data_value.split(".")
+    def __validate_format(self):
+        sequences = self.dataValue.split(".")
         if len(sequences[0]) == 5:
             return {"status": self.SUCCESS}
         return {
             "status": self.FAILURE,
             "message": "Coordinates should have 5 decimal points.",
-        }
-
-    def validate_restrictions(self):
-        if self.record.longitude != "" and self.record.latitude != "":
-            return {"status": self.SUCCESS}
-        return {
-            "status": self.FAILURE,
-            "message": "Longitude or latitude missing.",
         }
