f5cdbe238f6f8424595c80d0ee659195b54e5087
David Rosado <drosado@talusanalytics.com>
2022-10-25 14:46:52 -0400

    removing methods for merging records

--- src/lambda/save_register/validator/type.py
+++ src/lambda/save_register/validator/type.py
@@ -38,14 +38,6 @@ class Datapoint:
             previous = Datapoint(self.previous)
             self.previous = previous
 
-    def __hash__(self) -> int:
-        return hash(self.timestamp)
-
-    def __eq__(self, __o) -> bool:
-        return (
-            isinstance(__o, Datapoint) and self.get_datapoint() == __o.get_datapoint()
-        )
-
     def get_datapoint(self) -> dict:
         return {s: getattr(self, s) for s in self.__slots__ if hasattr(self, s)}
 
@@ -61,50 +53,3 @@ class Record:
     def __init__(self, record: dict):
         datapoints = {key: Datapoint(value) for key, value in record.items()}
         self.__dict__.update(datapoints)
-
-    @staticmethod
-    def __unpack_datapoint(datapoint: Datapoint) -> set:
-        unpacked_datapoints = set()
-        while hasattr(datapoint, "previous"):
-            unpacked_datapoints.add(datapoint)
-            datapoint = datapoint.previous
-        return unpacked_datapoints
-
-    @staticmethod
-    def __order_datapoints(datapoints: list) -> Datapoint:
-        # Sort list from oldest to latest timestamp
-        sorted(datapoints, key=lambda datapoint: datapoint.timestamp, reverse=True)
-        # Extract the latest datapoint
-        oldest = datapoints.pop(0)
-        # Nest datapoints
-        while datapoints:
-            latest = datapoints.pop(0)
-            latest.preview = oldest
-            oldest = latest
-        return oldest
-
-    def __iadd__(self, __record):
-        """
-        Updates the record by overloading the operator +=.
-        This can be achieved with: record1 += record2. Note that only record1 is modified.
-        """
-        # # Client record
-        client = {
-            key: self.__unpack_datapoint(value) for key, value in self.__dict__.items()
-        }
-        # Stored record
-        stored = {
-            key: self.__unpack_datapoint(value)
-            for key, value in __record.__dict__.items()
-        }
-        # Merge two dictionaries with common and shared keys
-        merged_record = {
-            k: client[k].union(stored[k])
-            if (k in client and k in stored)
-            else client.get(k) or stored.get(k)
-            for k in sorted(client.keys() | stored.keys())
-        }
-        # Nest datapoints and update the record
-        self.__dict__.update(
-            {k: self.__order_datapoints(list(v)) for k, v in merged_record.items()}
-        )
