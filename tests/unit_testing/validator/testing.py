19fadb8d69466297b3ba8e79deca8171d4540b6b
David Rosado <drosado@talusanalytics.com>
2022-10-26 09:38:11 -0400

    testing validation

--- tests/unit_testing/validator/testing.py
+++ tests/unit_testing/validator/testing.py
@@ -1,7 +1,7 @@
 import sys
 
 sys.path.append("./src/lambda/save_register/validator/")
-from type import Datapoint, Record
+from type import Datapoint
 from latin import Latin
 
 # Datapoint
@@ -21,3 +21,10 @@ host_species = {
 # Testing datapoint equality
 datapoint = Datapoint(host_species)
 assert datapoint.get_datapoint() == host_species
+
+# Testing validation
+assert not hasattr(datapoint, "report")
+Latin(datapoint)
+assert hasattr(datapoint, "report")
+assert "status" in datapoint.report
+assert datapoint.report["status"] == "SUCCESS"
