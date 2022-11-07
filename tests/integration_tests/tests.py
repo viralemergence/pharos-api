286183648f1679a325d5944ac6699452c89814bb
David Rosado <drosado@talusanalytics.com>
2022-11-07 15:33:29 -0500

    integration test skeleton pytest

---
+++ tests/integration_tests/tests.py
@@ -0,0 +1,38 @@
+import json
+import requests
+import pytest
+
+
+def test_save_register_one_record():
+    """Save a register with only one record."""
+    pass
+
+
+def test_load_register_one_record():
+    """Load a register with one record."""
+    pass
+
+
+def test_publish_register_one_record():
+    """Publish a register with one record."""
+    pass
+
+
+def test_save_register_multiple_record():
+    """Save a register with multiple records."""
+    pass
+
+
+def test_load_register_multiple_record():
+    """Load a register with multiple records."""
+    pass
+
+
+def test_publish_register_multiple_record():
+    """Publish a regster with multiple records"""
+    pass
+
+
+def test_retrieve_database():
+    """Retrieve all the records in the Records table."""
+    pass
