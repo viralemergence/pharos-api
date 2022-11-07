353c751fe778c96ac7d52fab0b62818b1b4728ed
David Rosado <drosado@talusanalytics.com>
2022-11-07 13:22:30 -0500

    publishing mechanism

--- src/lambda/publish_register/app.py
+++ src/lambda/publish_register/app.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import sessionmaker
 
 # from auth import check_auth
 from format import format_response
-from tables import TestTable, AnimalTable
+from tables import Records, ResearcherRecords, create_record
 
 
 RDS = boto3.client("rds")
@@ -15,6 +15,7 @@ HOST = os.environ["HOST"]
 PORT = os.environ["PORT"]
 USERNAME = os.environ["USERNAME"]
 DATABASE = os.environ["DATABASE"]
+PASSWORD = os.environ["PASSWORD"]
 
 
 def lambda_handler(event, _):
@@ -23,31 +24,37 @@ def lambda_handler(event, _):
 
     try:
 
-        database_token = RDS.generate_db_auth_token(
-            DBHostname=HOST,
-            Port=PORT,
-            DBUsername=USERNAME,
-        )
-
         database_url = URL.create(
             drivername="postgresql+psycopg2",
             host=HOST,
             database=DATABASE,
             username=USERNAME,
             port=PORT,
-            password=database_token,
+            password=PASSWORD,
             query={"sslmode": "verify-full", "sslrootcert": "./AmazonRootCA1.pem"},
         )
 
         engine = sqlalchemy.create_engine(database_url)
 
         Session = sessionmaker(bind=engine)
-        session = Session()
 
-        # session.add()  # Here use post_data
-        # session.commit()
+        Records.__table__.create(engine, checkfirst=True)
+        ResearcherRecords.__table__.create(engine, checkfirst=True)
 
-        session.close()
+        with Session() as session:
+
+            records = []
+
+            for record_id, record in post_data["register"].items():
+                id_ = f"{post_data['projectID']}-{post_data['datasetID']}-{record_id}"
+                pharos_record, researcher_record = create_record(
+                    post_data["researcherID"], id_, record
+                )
+                records.extend([pharos_record, researcher_record])
+
+            session.add_all(records)
+            session.commit()
+
         return format_response(200, "Works")
 
     except Exception as e:
