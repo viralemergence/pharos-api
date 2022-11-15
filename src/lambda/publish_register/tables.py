db870bbce640c7ea29969f88e99b3ce1c5eac397
David Rosado <drosado@talusanalytics.com>
2022-11-15 14:44:16 -0500

    refactor

--- src/lambda/publish_register/tables.py
+++ src/lambda/publish_register/tables.py
@@ -29,7 +29,7 @@ class Records(Base):
     record_id = Column(String(20))
     test_id = Column(String(25))
     sample_id = Column(String(25))
-    pool = Column(Boolean)
+    # pool = Column(Boolean)
     animal_id = Column(String(32))
     collection_method = Column(String(50))
     detection_method = Column(String(50))
@@ -39,15 +39,15 @@ class Records(Base):
     pathogen_ncbi_tax_id = Column(String(8))
     detection_outcome = Column(String(12))
     location = Column(Geometry("Point"))
-    precision = Column(Numeric(1, 0))  # 1 meter scale
+    spatial_uncertainity = Column(Numeric(1, 0))  # 1 meter scale
     collection_date = Column(Date())
     animal_identifier = Column(String(40))
     host_species = Column(String(50))
     host_ncbi_tax_id = Column(String(8))
-    host_uncertanity = Column(String(15))
+    # host_uncertanity = Column(String(15))
     life_stage = Column(String(15))
-    sex = Column(String(1))  # M, F, U
-    alive = Column(String(7))  # Y, N, U
+    organism_sex = Column(String(1))  # M, F, U
+    dead_or_alive = Column(String(7))  # Y, N, U
     age = Column(BigInteger())  # age units seconds 100 years ==> integer of 10 units
     mass = Column(Numeric(7, 6))  # mass units to kg 0.000000
     length = Column(Numeric(7, 6))  # length units to meters 0.000000
@@ -66,7 +66,7 @@ def create_records(pharosId, researcherId, record):
         record_id=rec,
         test_id=record["Test ID"]["dataValue"],
         sample_id=record["Sample ID"]["dataValue"],
-        pool=1,  # record["Pool"]["dataValue"],
+        # pool=1,  # record["Pool"]["dataValue"],
         animal_id=record["Animal ID or nickname"]["dataValue"],
         collection_method=record["Collection method or tissue"]["dataValue"],
         detection_method=record["Detection method"]["dataValue"],
@@ -78,16 +78,16 @@ def create_records(pharosId, researcherId, record):
         pathogen_ncbi_tax_id=record["Pathogen NCBI Tax ID"]["dataValue"],
         detection_outcome=record["Detection outcome"]["dataValue"],
         location=f"POINT({record['Longitude']['dataValue']} {record['Latitude']['dataValue']})",
-        precision=1,  # record["Precision"]["dataValue"],
+        spatial_uncertainity=record["Spatial uncertainity"]["dataValue"],
         collection_date=datetime.datetime.strptime(
             record["Collection date"]["dataValue"], "%d/%m/%Y"
         ).date(),  # datetime.datetime( f"{record["Year"]}/{record["Month"]}/{record["Day"]}", '%y/%m/%d')
         host_species=record["Host species"]["dataValue"],
         host_ncbi_tax_id=record["Host NCBI Tax ID"]["dataValue"],
-        host_uncertanity="",  # record["Uncertaninity"]["dataValue"],
+        # host_uncertanity="",  # record["Uncertaninity"]["dataValue"],
         life_stage=record["Life stage"]["dataValue"],
-        sex=record["Organism sex"]["dataValue"],
-        alive=record["Dead or alive"]["dataValue"],
+        organism_sex=record["Organism sex"]["dataValue"],
+        dead_or_alive=record["Dead or alive"]["dataValue"],
         age=float(record["Age"]["dataValue"]),
         mass=float(record["Mass"]["dataValue"]),
         length=float(record["Length"]["dataValue"]),
