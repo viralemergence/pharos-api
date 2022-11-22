14dd4a4b5016c4c0109e16b12ad0ea3b57a7b6d1
David Rosado <drosado@talusanalytics.com>
2022-11-22 14:40:30 -0500

    not required fields default to NULL

--- src/lambda/publish_project/models.py
+++ src/lambda/publish_project/models.py
@@ -58,42 +58,42 @@ class Tests(Base):
     dataset_id = Column(String(20))
     record_id = Column(String(20))
     row_id = Column(String(25))
-    sample_id = Column(String(25))
-    animal_id = Column(String(32))
+    sample_id = Column(String(25), default=None)
+    animal_id = Column(String(32), default=None)
     host_species = Column(String(50))
-    host_ncbi_tax_id = Column(String(8))
+    host_ncbi_tax_id = Column(String(8), default=None)
     location = Column(Geometry("Point"))  # latitude, longitude
     spatial_uncertainty = Column(
-        StringToNumeric(1, 0)
+        StringToNumeric(1, 0), default=None
     )  # 1 meter scale / spatial uncertainity and spatial uncertainity units
     collection_date = Column(
         Date()
     )  # collection day, collection month, collection year
-    collection_method_or_tissue = Column(String(50))
-    detection_method = Column(String(50))
+    collection_method_or_tissue = Column(String(50), default=None)
+    detection_method = Column(String(50), default=None)
     # primer_sequence
     # primer_citation
-    detection_target = Column(String(50))
-    detection_target_ncbi_tax_id = Column(String(8))
-    detection_outcome = Column(String(12))
+    detection_target = Column(String(50), default=None)
+    detection_target_ncbi_tax_id = Column(String(8), default=None)
+    detection_outcome = Column(String(12), default=None)
     # detection_measurement
     # detection_measurement_units
     pathogen = Column(String(50))
-    pathogen_ncbi_tax_id = Column(String(8))
+    pathogen_ncbi_tax_id = Column(String(8), default=None)
     # genbank_accession
     # detection_comments
-    organism_sex = Column(String(1))  # M, F, U
-    dead_or_alive = Column(String(7))  # Y, N, U
+    organism_sex = Column(String(1), default=None)  # M, F, U
+    dead_or_alive = Column(String(7), default=None)  # Y, N, U
     # health_notes
-    life_stage = Column(String(15))
+    life_stage = Column(String(15), default=None)
     age = Column(
-        StringToInteger()
+        StringToInteger(), default=None
     )  # age units seconds 100 years ==> integer of 10 digits / age, age units
     mass = Column(
-        StringToNumeric(12, 6)
+        StringToNumeric(12, 6), default=None
     )  # mass units to kg xxxxxx.xxxxxx / mass, mass units / tons - mg
     length = Column(
-        StringToNumeric(8, 6)
+        StringToNumeric(8, 6), default=None
     )  # length units to meters xx.xxxxxx / length, length units / m - mm
     # pool = Column(Boolean)
     # host_uncertanity = Column(String(15))
