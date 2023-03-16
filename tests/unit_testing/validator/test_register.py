536bad48dcb5e83cf211295431de0d97a9ed53bf
Ryan Zimmerman <rimmerman@talusanalytics.com>
2023-03-16 20:37:56 -0300

    timing register validation

--- tests/unit_testing/validator/test_register.py
+++ tests/unit_testing/validator/test_register.py
@@ -1,33 +1,52 @@
 import sys
 import json
+import timeit
 
 
 sys.path.append("./src/lambda/save_register/")
 from validator.definitions import Datapoint, Record
 from validator.validate_record import validate_record
 
-with open("./tests/unit_testing/validator/register.json") as file:
-    register = json.load(file)
+with open("./valid_test_register.json") as file:
+    data = file.read()
+    # register = json.load(file)
 
-register_ = {}
 
-for key, value in register.items():
-    record = Record(value, key)
-    register_[record.id] = record.get_record()
+def old_validate_register(json_data: str):
+    register = json.loads(json_data)
 
-assert register_ == register
+    verified_register = {}
 
-valid_register = {}
+    for record_id, record in register["register"].items():
+        record_ = Record(record, record_id)
+        record_ = validate_record(record_)
+        verified_register[record_id] = record_.get_record()
 
-for record_id, record in register.items():
-    record_ = Record(record, record_id)
-    valid_rec = validate_record(record_)
+    return verified_register
 
-    valid_register[record_id] = valid_rec.get_record()
 
-assert valid_register != register_
-assert valid_register != register
+valid_register = old_validate_register(data)
 
-for record in valid_register.values():
-    for k, dp in record.items():
-        assert "report" in dp
+register_time = timeit.timeit(lambda: old_validate_register(data), number=10)
+
+print(register_time)
+
+# print(valid_register["recH6U0vPQPHL"])
+
+
+# assert register_ == register
+
+# valid_register = {}
+
+# for record_id, record in register.items():
+#     record_ = Record(record, record_id)
+#     valid_rec = validate_record(record_)
+
+#     valid_register[record_id] = valid_rec.get_record()
+
+# assert valid_register != register_
+# assert valid_register != register
+
+# for record in valid_register.values():
+#     for k, dp in record.items():
+#         assert "report" in dp
