fe8704c3eba4b1f9ecc24d08afbe3e6ff74640b6
ryan-talus <rzimmerman@talusanalytics.com>
2023-03-19 18:29:35 -0300

    Adding type guard back in after testing it in CCI

--- src/lambda/list_projects/app.py
+++ src/lambda/list_projects/app.py
@@ -21,15 +21,18 @@ def lambda_handler(event, _):
         user = USERS_TABLE.get_item(Key={"researcherID": post_data["researcherID"]})
         projectids = user["Item"]["projectIDs"]
 
-        # if isinstance(projectids, list):
-        projects = DYNAMODB.batch_get_item(
-            RequestItems={
-                PROJECTS_TABLE: {
-                    "Keys": [{"projectID": projectID} for projectID in projectids]
+        if isinstance(projectids, list):
+            projects = DYNAMODB.batch_get_item(
+                RequestItems={
+                    PROJECTS_TABLE: {
+                        "Keys": [{"projectID": projectID} for projectID in projectids]
+                    }
                 }
-            }
-        )
-        return format_response(200, projects["Responses"][PROJECTS_TABLE])
+            )
+
+            return format_response(200, projects["Responses"][PROJECTS_TABLE])
+
+        return format_response(500, "Projects not found")
 
     except Exception as e:  # pylint: disable=broad-except
         return format_response(403, e)
