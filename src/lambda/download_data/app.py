fa3d936733487653772ac956290fd687cc277700
ryan-talus <rzimmerman@talusanalytics.com>
2023-03-19 17:09:39 -0300

    Commenting out this code for review later (want to get coding standards working in CI)

--- src/lambda/download_data/app.py
+++ src/lambda/download_data/app.py
@@ -1,61 +1,61 @@
-import os
-import json
-import datetime
-import boto3
-import sqlalchemy
-from sqlalchemy.engine import URL
-from auth import check_auth
-from format import format_response
+# import os
+# import json
+# import datetime
+# import boto3
+# import sqlalchemy
+# from sqlalchemy.engine import URL
+# from auth import check_auth
+# from format import format_response
 
-RDS = boto3.client("rds")
-SECRETS_MANAGER = boto3.client("secretsmanager", region_name="us-west-1")
+# RDS = boto3.client("rds")
+# SECRETS_MANAGER = boto3.client("secretsmanager", region_name="us-west-1")
 
-DATABASE = os.environ["DATABASE"]
-REGION = os.environ["REGION"]
-BUCKET = os.environ["BUCKET"]
+# DATABASE = os.environ["DATABASE"]
+# REGION = os.environ["REGION"]
+# BUCKET = os.environ["BUCKET"]
 
-response = SECRETS_MANAGER.get_secret_value(SecretId=DATABASE)
-CREDENTIALS = json.loads(response["SecretString"])
+# response = SECRETS_MANAGER.get_secret_value(SecretId=DATABASE)
+# CREDENTIALS = json.loads(response["SecretString"])
 
 
-def lambda_handler(event, _):
+# def lambda_handler(event, _):
 
-    post_data = json.loads(event.get("body", "{}"))
+#     post_data = json.loads(event.get("body", "{}"))
 
-    authorized = check_auth(post_data["researcherID"])
+#     authorized = check_auth(post_data["researcherID"])
 
-    if not authorized:
-        return format_response(403, "Not Authorized")
+#     if not authorized:
+#         return format_response(403, "Not Authorized")
 
-    try:
+#     try:
 
-        database_url = URL.create(
-            drivername="postgresql+psycopg2",
-            host=CREDENTIALS["host"],
-            database=DATABASE,
-            username=CREDENTIALS["username"],
-            port=CREDENTIALS["port"],
-            password=CREDENTIALS["password"],
-            query={"sslmode": "verify-full", "sslrootcert": "./AmazonRootCA1.pem"},
-        )
+#         database_url = URL.create(
+#             drivername="postgresql+psycopg2",
+#             host=CREDENTIALS["host"],
+#             database=DATABASE,
+#             username=CREDENTIALS["username"],
+#             port=CREDENTIALS["port"],
+#             password=CREDENTIALS["password"],
+#             query={"sslmode": "verify-full", "sslrootcert": "./AmazonRootCA1.pem"},
+#         )
 
-        engine = sqlalchemy.create_engine(database_url)
+#         engine = sqlalchemy.create_engine(database_url)
 
-        connection = engine.connect()
+#         connection = engine.connect()
 
-        date = datetime.datetime.utcnow().date()
+#         date = datetime.datetime.utcnow().date()
 
-        file_path = f"data_{datetime.datetime.strftime(date, '%Y_%m_%d')}"
+#         file_path = f"data_{datetime.datetime.strftime(date, '%Y_%m_%d')}"
 
-        s3_uri = f"aws_commons.create_s3_uri('{BUCKET}', '{file_path}', '{REGION}')"
+#         s3_uri = f"aws_commons.create_s3_uri('{BUCKET}', '{file_path}', '{REGION}')"
 
-        connection.execute(
-            f"SELECT * from aws_s3.query_export_to_s3('SELECT * FROM records', {s3_uri}, options :='format csv');"
-        )
+#         connection.execute(
+#             f"SELECT * from aws_s3.query_export_to_s3('SELECT * FROM records', {s3_uri}, options :='format csv');"
+#         )
 
-        connection.close()
+#         connection.close()
 
-        return format_response(200, "Data downloaded")
+#         return format_response(200, "Data downloaded")
 
-    except Exception as e:
-        return format_response(403, e)
+#     except Exception as e:
+#         return format_response(403, e)
--- src/lambda/publish_project/create_record.py
+++ src/lambda/publish_project/create_record.py
@@ -1,49 +1,49 @@
-import datetime
-from models import Tests, ResearchersTests
+# import datetime
+# from models import Tests, ResearchersTests
 
 
-def verify_record(record: dict) -> bool:
+# def verify_record(record: dict) -> bool:
 
-    for datapoint in record.values():
-        if datapoint["report"]["status"] == "FAIL":
-            return False
+#     for datapoint in record.values():
+#         if datapoint["report"]["status"] == "FAIL":
+#             return False
 
-    return True
-
-
-def preprocess_record(record, record_id, dataset_id, project_id) -> dict:
-    # Keys to lowercase and unpack datavalue
-    record_ = {k.lower().replace(" ", "_"): v["dataValue"] for k, v in record.items()}
-    # Create geometry point
-    record_.update({"location": f"POINT({record_['longitude']} {record_['latitude']})"})
-    # Create date
-    record_.update(
-        {
-            "collection_date": datetime.datetime.strptime(
-                f"{record_['collection_day']}/{record_['collection_month']}/{record_['collection_year']}",
-                "%d/%m/%Y",
-            ).date()
-        }
-    )
-    # Add pharos id
-    record_.update(
-        {
-            "record_id": record_id,
-            "dataset_id": dataset_id,
-            "project_id": project_id,
-            "pharos_id": f"{project_id}-{dataset_id}-{record_id}",
-        }
-    )
-
-    return record_
+#     return True
 
 
-def create_records(project_id, dataset_id, record_id, researcher_id, record):
+# def preprocess_record(record, record_id, dataset_id, project_id) -> dict:
+#     # Keys to lowercase and unpack datavalue
+#     record_ = {k.lower().replace(" ", "_"): v["dataValue"] for k, v in record.items()}
+#     # Create geometry point
+#     record_.update({"location": f"POINT({record_['longitude']} {record_['latitude']})"})
+#     # Create date
+#     record_.update(
+#         {
+#             "collection_date": datetime.datetime.strptime(
+#                 f"{record_['collection_day']}/{record_['collection_month']}/{record_['collection_year']}",
+#                 "%d/%m/%Y",
+#             ).date()
+#         }
+#     )
+#     # Add pharos id
+#     record_.update(
+#         {
+#             "record_id": record_id,
+#             "dataset_id": dataset_id,
+#             "project_id": project_id,
+#             "pharos_id": f"{project_id}-{dataset_id}-{record_id}",
+#         }
+#     )
 
-    researcher_test_record = ResearchersTests(
-        researcher_id=researcher_id, record_id=f"{project_id}-{dataset_id}-{record_id}"
-    )
+#     return record_
 
-    test_record = Tests(**preprocess_record(record, record_id, dataset_id, project_id))
 
-    return test_record, researcher_test_record
+# def create_records(project_id, dataset_id, record_id, researcher_id, record):
+
+#     researcher_test_record = ResearchersTests(
+#         researcher_id=researcher_id, record_id=f"{project_id}-{dataset_id}-{record_id}"
+#     )
+
+#     test_record = Tests(**preprocess_record(record, record_id, dataset_id, project_id))
+
+#     return test_record, researcher_test_record
