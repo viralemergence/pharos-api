a169fb4197d628184d2c242524aaeb079368af25
ryan-talus <rzimmerman@talusanalytics.com>
2023-04-09 10:40:36 -0300

    refactoring auth for new table structure

--- src/lambda/auth/app.py
+++ src/lambda/auth/app.py
@@ -1,21 +1,22 @@
 import os
 import json
 import boto3
+from botocore.exceptions import ClientError
 from format import format_response
 
 
 DYNAMODB = boto3.resource("dynamodb")
-USERS_TABLE = DYNAMODB.Table(os.environ["USERS_TABLE_NAME"])
+METADATA_TABLE = DYNAMODB.Table(os.environ["METADATA_TABLE_NAME"])
 
 
 def lambda_handler(event, _):
     post_data = json.loads(event.get("body", "{}"))
     try:
-        users_response = USERS_TABLE.get_item(
-            Key={"researcherID": post_data["researcherID"]}
+        users_response = METADATA_TABLE.get_item(
+            Key={"pk": post_data["researcherID"], "sk": "_meta"}
         )
 
-    except Exception as e:  # pylint: disable=broad-except
+    except ClientError as e:
         return format_response(500, e)
 
     if "Item" in users_response:
