d959a5c634d3054649328909307c5f8db463c99f
ryan-talus <rzimmerman@talusanalytics.com>
2023-04-17 11:54:44 -0300

    Nailing down full proper mock data for publishing a register; this highlights the "name" vs "first_name" & "last_name" mismatch

--- tests/unit_testing/test_database.py
+++ tests/unit_testing/test_database.py
@@ -1,110 +1,177 @@
-"""Basic tests for transforming records into database tests"""
+"""Basic tests for publishing records to the database."""
 
 
 # from datetime import date
 # from typing import Union
 # from devtools import debug
-# from geoalchemy2 import load_spatialite
-# from sqlalchemy import create_engine, select
-# from sqlalchemy.event import listen
-# from sqlalchemy.orm import Session
-# from register import Datapoint, Record, ReportScore
+from geoalchemy2 import load_spatialite
+from sqlalchemy import create_engine, select
+from sqlalchemy.event import listen
+from sqlalchemy.orm import Session
+from register import (
+    Author,
+    Datapoint,
+    Dataset,
+    DatasetReleaseStatus,
+    Project,
+    ProjectAuthorRole,
+    ProjectPublishStatus,
+    Register,
+    User,
+)
 
-# from models2 import Attribution, PublishedRecord, Researcher, Base
+from models2 import Attribution, PublishedRecord, Researcher, Base
 
-# VALID_RECORD = """
-# {
-#     "Host species": {
-#         "dataValue": "Vulpes vulpes",
-#         "modifiedBy": "dev",
-#         "version": "2",
-#         "previous": {
-#             "dataValue": "Previous Data Value",
-#             "modifiedBy": "jane",
-#             "version": "1",
-#             "previous": {
-#                 "dataValue": "Old value",
-#                 "modifiedBy": "Nathan",
-#                 "version": "0"
-#             }
-#         }
-#     },
-#     "Latitude": {
-#         "dataValue": "40.0150",
-#         "modifiedBy": "dev",
-#         "version": "1679692123"
-#     },
-#     "Longitude": {
-#         "dataValue": "105.2705",
-#         "modifiedBy": "dev",
-#         "version": "1679692223"
-#     },
-#     "Collection day": {
-#         "dataValue": "1",
-#         "modifiedBy": "john",
-#         "version": "1679692123"
-#     },
-#     "Collection month": {
-#         "dataValue": "1",
-#         "modifiedBy": "dev",
-#         "version": "1679692123"
-#     },
-#     "Collection year": {
-#         "dataValue": "2019",
-#         "modifiedBy": "dev",
-#         "version": "1679692123"
-#     },
-#     "Detection outcome": {
-#         "dataValue": "positive",
-#         "modifiedBy": "dev",
-#         "version": "1679692123"
-#     },
-#     "Pathogen": {
-#         "dataValue": "SARS-CoV-2",
-#         "modifiedBy": "dev",
-#         "version": "1679692123"
-#     }
-# }
-# """
-
-# ENGINE = create_engine("sqlite+pysqlite:///:memory:", echo=True)
-# listen(ENGINE, "connect", load_spatialite)
-# Base.metadata.create_all(ENGINE)
-
-# JANE_ID = "A098SKHLSD234"
+MOCK_PROJECT_ID = "prjl90OaJvWZR"
 
 
-# def test_researcher():
-#     with Session(ENGINE) as session:
-#         researcher = Researcher(
-#             researcher_id=JANE_ID,
-#             first_name="Jane",
-#             last_name="Doe",
-#         )
-#         session.add(researcher)
-#         session.commit()
+JOHN_SMITH = User(
+    researcherID="resl90OaJvWZR",
+    name="John Smith",
+    email="john.smith@institute.org",
+    organization="Institute of Research",
+    projectIDs={MOCK_PROJECT_ID},
+)
 
-#     with Session(ENGINE) as session:
-#         result = session.scalars(select(Researcher)).one()
+JANE_DOE = User(
+    researcherID="resl90l123kxd",
+    name="Jane Doe",
+    email="jane.doe@institute.org",
+    organization="Institute of Research",
+    projectIDs={MOCK_PROJECT_ID},
+)
 
-#         print(result)
+JOHN_AUTHOR = Author(
+    researcherID=JOHN_SMITH.researcherID,
+    role=ProjectAuthorRole.ADMIN,
+)
 
-#         assert result.researcher_id == JANE_ID
-#         assert result.first_name == "Jane"
-#         assert result.last_name == "Doe"
+JANE_AUTHOR = Author(
+    researcherID=JANE_DOE.researcherID,
+    role=ProjectAuthorRole.ADMIN,
+)
 
 
-# PHAROS_ID = "prjl90OaJvWZR-setxlj1qoFxLC-recJSdfsklklo"
+MOCK_PROJECT = Project(
+    projectID=MOCK_PROJECT_ID,
+    name="Mock Project",
+    description="Test project description",
+    authors=[JANE_AUTHOR, JOHN_AUTHOR],
+    citation="Test citation",
+    datasetIDs=["setxlj1qoFxLC"],
+    lastUpdated="2021-01-01",
+    othersCiting=[""],
+    projectPublications=[""],
+    projectType="",
+    publishStatus=ProjectPublishStatus.UNPUBLISHED,
+    relatedMaterials=[""],
+    surveillanceStatus="Ongoing",
+)
 
-# JOHN_ID = "ASDF9098234SD"
+MOCK_DATASET = Dataset(
+    datasetID="setxlj1qoFxLC",
+    projectID=MOCK_PROJECT_ID,
+    releaseStatus=DatasetReleaseStatus.RELEASED,
+    name="Mock Dataset",
+    lastUpdated="2021-01-01",
+    earliestDate="2019-01-01",
+    latestDate="2020-01-01",
+)
+
+MOCK_REGISTER = Register.parse_raw(
+    """
+    {
+        "register": {
+            "recAS40712sdgl": {
+                "Host species": {
+                    "dataValue": "Vulpes vulpes",
+                    "modifiedBy": "dev",
+                    "version": "2",
+                    "previous": {
+                        "dataValue": "Previous Data Value",
+                        "modifiedBy": "jane",
+                        "version": "1",
+                        "previous": {
+                            "dataValue": "Old value",
+                            "modifiedBy": "Nathan",
+                            "version": "0"
+                        }
+                    }
+                },
+                "Latitude": {
+                    "dataValue": "40.0150",
+                    "modifiedBy": "dev",
+                    "version": "1679692123"
+                },
+                "Longitude": {
+                    "dataValue": "105.2705",
+                    "modifiedBy": "dev",
+                    "version": "1679692223"
+                },
+                "Collection day": {
+                    "dataValue": "1",
+                    "modifiedBy": "john",
+                    "version": "1679692123"
+                },
+                "Collection month": {
+                    "dataValue": "1",
+                    "modifiedBy": "dev",
+                    "version": "1679692123"
+                },
+                "Collection year": {
+                    "dataValue": "2019",
+                    "modifiedBy": "dev",
+                    "version": "1679692123"
+                },
+                "Detection outcome": {
+                    "dataValue": "positive",
+                    "modifiedBy": "dev",
+                    "version": "1679692123"
+                },
+                "Pathogen": {
+                    "dataValue": "SARS-CoV-2",
+                    "modifiedBy": "dev",
+                    "version": "1679692123"
+                }
+            }
+        }
+    }
+    """
+)
+
+ENGINE = create_engine("sqlite+pysqlite:///:memory:", echo=True)
+listen(ENGINE, "connect", load_spatialite)
+Base.metadata.create_all(ENGINE)
+
+JANE_ID = "A098SKHLSD234"
 
 
-# def researchers_from_datapoint(datapoint: Datapoint, researchers: set[str]) -> set[str]:
-#     if datapoint.previous is not None:
-#         researchers = researchers_from_datapoint(datapoint.previous, researchers)
+def test_researcher():
+    with Session(ENGINE) as session:
+        researcher = Researcher(
+            researcher_id=JANE_ID,
+            first_name="Jane",
+            last_name="Doe",
+        )
+        session.add(researcher)
+        session.commit()
 
-#     researchers.add(datapoint.modifiedBy)
-#     return researchers
+    with Session(ENGINE) as session:
+        result = session.scalars(select(Researcher)).one()
+
+        print(result)
+
+        assert result.researcher_id == JANE_ID
+        assert result.first_name == "Jane"
+        assert result.last_name == "Doe"
+
+
+def researchers_from_datapoint(datapoint: Datapoint, researchers: set[str]) -> set[str]:
+    if datapoint.previous is not None:
+        researchers = researchers_from_datapoint(datapoint.previous, researchers)
+
+    researchers.add(datapoint.modifiedBy)
+    return researchers
 
 
 # def test_transform_record():
