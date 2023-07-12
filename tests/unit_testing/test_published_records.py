344522841a7f2dce61aff7c4c45d5ea45693e390
Raphael Krut-Landau <raphael.kl@gmail.com>
2023-07-11 20:43:52 -0400

    Continue refactoring of tests of get_published_records
    Co-authored-by: Raphael Krut-Landau <raffi@bluetiger.digital>

--- tests/unit_testing/test_published_records.py
+++ tests/unit_testing/test_published_records.py
@@ -49,8 +49,12 @@ def create_mock_register(record_count: int) -> str:
 
         if index < 50:
             collection_year = "2023"
-        else:
+        elif index < 100:
             collection_year = "2024"
+        elif index < 150:
+            collection_year = "2025"
+        else:
+            collection_year = "2026"
 
         if index < 80:
             detection_outcome = "positive"
@@ -268,75 +272,95 @@ def cleanup(session):
     session.commit()
 
 
-def assert_filter(params, expected_record_count):
+def check(params, expected_record_count):
     assert len(get_query(ENGINE, params).all()) == expected_record_count
 
 
 def test_no_filters(mock_data):
-    assert_filter({}, 400)
+    check({}, 400)
 
 
-def assert_filter_by_pharos_id(mock_data):
-    assert_filter({"pharos_id": "project0-dataset0-rec0"}, 1)
-    assert_filter(
-        {"pharos_id": ["project0-dataset0-rec0", "project1-dataset1-rec1"]}, 2
+def test_filter_by_pharos_id(mock_data):
+    check({"pharos_id": "project0-dataset0-rec0"}, 1)
+    check({"pharos_id": ["project0-dataset0-rec0", "project1-dataset1-rec1"]}, 2)
+    check(
+        {"pharos_id": [f"project0-dataset0-rec{num}" for num in range(25)]},
+        25,
     )
 
 
-def assert_filter_by_host_species(mock_data):
-    assert_filter({"host_species": "host1"}, 300)
-    assert_filter({"host_species": "host2"}, 60)
-    assert_filter({"host_species": "host3"}, 40)
-    assert_filter({"host_species": ["host1", "host2"]}, 360)
-    assert_filter({"host_species": ["host2", "host3"]}, 100)
-    assert_filter({"host_species": ["host1", "host3"]}, 340)
+def test_filter_by_host_species(mock_data):
+    check({"host_species": "host1"}, 300)
+    check({"host_species": "host2"}, 60)
+    check({"host_species": "host3"}, 40)
+    check({"host_species": ["host1", "host2"]}, 360)
+    check({"host_species": ["host2", "host3"]}, 100)
+    check({"host_species": ["host1", "host3"]}, 340)
 
 
-def assert_filter_by_project_name(mock_data):
-    # the project name filter is case insensitive
-    assert_filter({"project_name": "project zero"}, 200)
+def test_filter_by_project_name(mock_data):
+    check({"project_name": "Project Zero"}, 200)
+    # case insensitive
+    check({"project_name": "project zero"}, 200)
     # the whole string must match
-    assert_filter({"project_name": "Zero"}, 0)
+    check({"project_name": "Zero"}, 0)
 
 
-def assert_filter_by_pathogen(mock_data):
-    assert_filter({"pathogen": "path1"}, 220)
-    assert_filter({"pathogen": "path2"}, 60)
-    assert_filter({"pathogen": "path3"}, 120)
-    assert_filter({"pathogen": ["path1", "path2"]}, 280)
-    assert_filter({"pathogen": ["path2", "path3"]}, 180)
-    assert_filter({"pathogen": ["path1", "path3"]}, 340)
+def test_filter_by_pathogen(mock_data):
+    check({"pathogen": "path1"}, 220)
+    check({"pathogen": "path2"}, 60)
+    check({"pathogen": "path3"}, 120)
+    check({"pathogen": ["path1", "path2"]}, 280)
+    check({"pathogen": ["path2", "path3"]}, 180)
+    check({"pathogen": ["path1", "path3"]}, 340)
 
 
-def assert_filter_by_project_name_and_host_species(mock_data):
-    assert_filter({"project_name": "Project Zero", "host_species": "host1"}, 150)
-    assert_filter({"project_name": "Project Zero", "host_species": "host2"}, 30)
-    assert_filter({"project_name": "Project Zero", "host_species": "host3"}, 20)
-    assert_filter(
-        {"project_name": "Project Zero", "host_species": ["host1", "host2"]}, 180
+def test_filter_by_project_name_and_host_species(mock_data):
+    check({"project_name": "Project Zero", "host_species": "host1"}, 150)
+    check({"project_name": "Project Zero", "host_species": "host2"}, 30)
+    check({"project_name": "Project Zero", "host_species": "host3"}, 20)
+    check({"project_name": "Project Zero", "host_species": ["host1", "host2"]}, 180)
+    check({"project_name": "Project Zero", "host_species": ["host2", "host3"]}, 50)
+    check({"project_name": "Project Zero", "host_species": ["host1", "host3"]}, 170)
+
+
+def test_filter_by_host_species_and_pathogen(mock_data):
+    check({"host_species": ["host1", "host2"], "pathogen": ["path2", "path3"]}, 140)
+
+
+def test_filter_by_collection_date(mock_data):
+    check({"collection_end_date": "2024-1-2"}, 200)
+    check({"collection_start_date": "2024-1-2"}, 200)
+
+
+def test_filter_by_collection_date_and_host_species(mock_data):
+    check({"collection_start_date": "2023-12-31", "host_species": "host1"}, 200)
+    check({"collection_end_date": "2023-1-2", "host_species": "host2"}, 0)
+
+
+def test_filter_by_researcher_name(mock_data):
+    check({"researcher": "Researcher Zero"}, 200)
+    check({"researcher": "Researcher One"}, 200)
+    check({"researcher": "Researcher Two"}, 200)
+    check({"researcher": "Researcher Three"}, 200)
+
+
+def test_filter_by_researcher_name_and_project_name(mock_data):
+    check({"researcher": "Researcher Zero", "project_name": "Project Zero"}, 200)
+    check({"researcher": "Researcher Zero", "project_name": "Project One"}, 0)
+
+
+# TODO finish this big test
+def skip__test_all_filters(mock_data):
+    check(
+        {
+            "pharos_id": [f"project0-dataset0-rec{num}" for num in range(70)]
+            + [f"project1-dataset1-rec{num}" for num in range(80)],
+            "project_name": "Project Zero",
+            "host_species": ["host1", "host2"],
+            "detection_outcome": ["host1", "host2"],
+            "collection_start_date": "2023-6-1",
+            "collection_end_date": "2024-6-1",
+        },
+        20,
     )
-    assert_filter(
-        {"project_name": "Project Zero", "host_species": ["host2", "host3"]}, 50
-    )
-    assert_filter(
-        {"project_name": "Project Zero", "host_species": ["host1", "host3"]}, 170
-    )
-
-    assert_filter(
-        {"host_species": ["host1", "host2"], "pathogen": ["path2", "path3"]}, 140
-    )
-
-    assert_filter({"collection_end_date": "2023-1-2"}, 100)
-    assert_filter({"collection_start_date": "2023-12-31"}, 300)
-    assert_filter({"collection_start_date": "2023-12-31", "host_species": "host1"}, 200)
-    assert_filter({"collection_end_date": "2023-1-2", "host_species": "host2"}, 0)
-
-    assert_filter({"researcher": "Researcher Zero"}, 200)
-    assert_filter({"researcher": "Researcher One"}, 200)
-    assert_filter({"researcher": "Researcher Two"}, 200)
-    assert_filter({"researcher": "Researcher Three"}, 200)
-
-    assert_filter(
-        {"researcher": "Researcher Zero", "project_name": "Project Zero"}, 200
-    )
-    assert_filter({"researcher": "Researcher Zero", "project_name": "Project One"}, 0)
