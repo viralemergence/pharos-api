f0d6a88adb08a6e058c3a92cddcb4619a8b43be9
Ryan Zimmerman <61983809+ryan-talus@users.noreply.github.com>
2023-07-13 20:25:44 +0100

    First sketch at a function to grab a project by project_id

--- src/lambda/get_published_project/app.py
+++ src/lambda/get_published_project/app.py
@@ -13,9 +13,42 @@ from register import COMPLEX_FIELDS
 SECRETS_MANAGER = boto3.client("secretsmanager", region_name="us-west-1")
 
 
+class QueryStringParameters(BaseModel):
+    project_id: str = Field(..., alias="projectID")
+
+    class Config:
+        extra = Extra.forbid
+
+class GetPublishedProjectEvent(BaseModel:
+    query_string_parameters: QueryStringParameters = Field(
+        ..., alias="queryStringParameters"
+    )
+
+    class Config: 
+        extra = Extra.ignore
+
 
 def lambda_handler(event, _):
 	try:
-        validated = GetPublishedRecordsEvent.parse_obj(event)
+        validated = GetPublishedProjectEvent.parse_obj(event)
     except ValidationError as e:
         return format_response(400, e.json(), preformatted=True)
+
+    engine = get_engine()
+
+    project_id = validated.query_string_parameters.project_id
+
+    with Session(engine) as session: 
+        project = session.query(
+            PublishedProject
+        ).filter(
+            PublishedProject.project_id == project_id
+        )
+
+    return format_response(200, { "project": project })
+
+
+
+
+
+
