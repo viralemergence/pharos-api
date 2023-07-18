732bdae18ae9e92bac5fba3b47511ee6471c64bd
ryan-talus <rzimmerman@talusanalytics.com>
2023-07-18 17:35:56 -0300

    Fixing typos in the getpublishedproject function

--- src/lambda/get_published_project/app.py
+++ src/lambda/get_published_project/app.py
@@ -1,13 +1,12 @@
 import boto3
 from pydantic import BaseModel, Extra, Field, ValidationError
+from sqlalchemy import select
 
 from sqlalchemy.orm import Session
-from column_alias import API_NAME_TO_UI_NAME_MAP
 from engine import get_engine
 
 from format import format_response
-from models import PublishedRecord
-from register import COMPLEX_FIELDS
+from models import PublishedProject
 
 
 SECRETS_MANAGER = boto3.client("secretsmanager", region_name="us-west-1")
@@ -19,17 +18,18 @@ class QueryStringParameters(BaseModel):
     class Config:
         extra = Extra.forbid
 
-class GetPublishedProjectEvent(BaseModel:
+
+class GetPublishedProjectEvent(BaseModel):
     query_string_parameters: QueryStringParameters = Field(
-        ..., alias="queryStringParameters"
+        alias="queryStringParameters"
     )
 
-    class Config: 
+    class Config:
         extra = Extra.ignore
 
 
 def lambda_handler(event, _):
-	try:
+    try:
         validated = GetPublishedProjectEvent.parse_obj(event)
     except ValidationError as e:
         return format_response(400, e.json(), preformatted=True)
@@ -38,17 +38,11 @@ def lambda_handler(event, _):
 
     project_id = validated.query_string_parameters.project_id
 
-    with Session(engine) as session: 
-        project = session.query(
-            PublishedProject
-        ).filter(
-            PublishedProject.project_id == project_id
+    with Session(engine) as session:
+        project = session.scalar(
+            select(PublishedProject).where(PublishedProject.project_id == project_id)
         )
 
-    return format_response(200, { "project": project })
+        print(project)
 
-
-
-
-
-
+    return format_response(200, {"project": project})
