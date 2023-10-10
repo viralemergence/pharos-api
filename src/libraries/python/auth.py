646320296dbfb990ef83f7284080480f52a5fcf3
ryan-talus <rzimmerman@talusanalytics.com>
2023-10-10 12:11:11 -0300

    Implementing auth claims check for auth and loading user

--- src/libraries/python/auth.py
+++ src/libraries/python/auth.py
@@ -1,6 +1,7 @@
 import os
 import boto3
 from botocore.exceptions import ClientError
+from pydantic import BaseModel, Field
 
 from register import User
 
@@ -8,7 +9,30 @@ DYNAMODB = boto3.resource("dynamodb")
 METADATA_TABLE = DYNAMODB.Table(os.environ["METADATA_TABLE_NAME"])
 
 
-def check_auth(researcher_id):
+class Claims(BaseModel):
+    sub: str
+    email_verified: str
+    iss: str
+    cognito_username: str = Field(alias="cognito:username")
+    origin_jti: str
+    aud: str
+    event_id: str
+    token_use: str
+    auth_time: str
+    exp: str
+    iat: str
+    jti: str
+    email: str
+
+
+def check_auth(lambda_event):
+
+    claims = Claims.parse_obj(
+        lambda_event.get("requestContext", {}).get("authorizer", {}).get("claims", {})
+    )
+
+    researcher_id = f"res{claims.sub}"
+
     try:
         users_response = METADATA_TABLE.get_item(
             Key={"pk": researcher_id, "sk": "_meta"}
