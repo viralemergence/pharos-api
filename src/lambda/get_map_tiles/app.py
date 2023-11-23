272298dcb60266996713ac24d8e6641ab83e8602
ryan-talus <rzimmerman@talusanalytics.com>
2023-11-23 18:54:05 -0300

    Pushing new map server to dev, with split api strategy
    One api for Text responses with Authorization, one api for binary responses
    without authorization.

--- src/lambda/get_map_tiles/app.py
+++ src/lambda/get_map_tiles/app.py
@@ -1,94 +1,97 @@
-# from dataclasses import dataclass
-# import time
+import base64
+from dataclasses import dataclass
 from sqlalchemy import select
 
 from sqlalchemy.orm import Session
-from sqlalchemy.types import STRINGTYPE
 
-from sqlalchemy.sql import cast, func
-from sqlalchemy.types import JSON
+from sqlalchemy.sql import func
+from urllib3.filepost import os
 from engine import get_engine
 
 from format import format_response
-from models import PublishedRecord
+from models import PublishedDataset, PublishedProject, PublishedRecord
 
 
-## wrote this for doing tiles;
-## commented out while this is only
-## returning geojson as proof of concept
+CORS_ALLOW = os.environ["CORS_ALLOW"]
 
-# @dataclass
-# class TilePath:
-#     """Validate and parse the path of a map tile request."""
+@dataclass
+class TilePath:
+    """Validate and parse the path of a map tile request."""
 
-#     z: int
-#     x: int
-#     y: int
+    z: int
+    x: int
+    y: int
 
-#     @classmethod
-#     def validate(cls, path):
-#         try:
-#             coords, extension = path.split(".")
-#             z, x, y = [int(chunk) for chunk in coords.split("/")]
+    @classmethod
+    def validate(cls, path):
+        try:
+            coords, extension = path.split(".")
+            z, x, y = [int(chunk) for chunk in coords.split("/")]
 
-#         except ValueError as exc:
-#             raise ValueError(
-#                 "Incorrect format; Map tile requests "
-#                 "must be in the format /map/z/x/y.pbf"
-#             ) from exc
+        except ValueError as exc:
+            raise ValueError(
+                "Incorrect format; Map tile requests "
+                "must be in the format /map/z/x/y.pbf"
+            ) from exc
 
-#         if extension != "pbf":
-#             raise ValueError(
-#                 f"Incorrect extension '.{extension}'; Map tile "
-#                 "requests must be in the format /map/z/x/y.pbf"
-#             )
+        if extension != "pbf":
+            raise ValueError(
+                f"Incorrect extension '.{extension}'; Map tile "
+                "requests must be in the format /map/z/x/y.pbf"
+            )
 
-#         min_z, max_z = 0, 20
-#         if not min_z <= int(z) <= max_z:
-#             raise ValueError(
-#                 "Unsupported zoom level; zoom level "
-#                 f"must be between {min_z} and {max_z}"
-#             )
+        min_z, max_z = 0, 20
+        if not min_z <= int(z) <= max_z:
+            raise ValueError(
+                "Unsupported zoom level; zoom level "
+                f"must be between {min_z} and {max_z}"
+            )
 
-#         return cls(z, x, y)
+        return cls(z, x, y)
 
 
-def lambda_handler(_, __):
-    # try:
-    #     tile_path = TilePath.validate(event["pathParameters"]["mapPath"])
-    # except ValueError as exc:
-    #     return format_response(400, {"error": str(exc)})
+def lambda_handler(event, _):
+    try:
+        tile_path = TilePath.validate(event["pathParameters"]["mapPath"])
+    except ValueError as exc:
+        return format_response(400, {"error": str(exc)})
 
     engine = get_engine()
-    with Session(engine) as session:
 
-        data = session.scalar(
+    with Session(engine) as session:
+        layer_name = "pharos-points"
+        mvt_geom = (
             select(
-                cast(
-                    func.json_build_object(
-                        "type",
-                        "FeatureCollection",
-                        "features",
-                        func.json_agg(
-                            cast(
-                                func.ST_AsGeoJSON(
-                                    select(
-                                        PublishedRecord.geom,
-                                        PublishedRecord.pharos_id,
-                                        PublishedRecord.sample_id,
-                                        PublishedRecord.collection_date,
-                                    ).subquery()
-                                ),
-                                JSON,
-                            )
-                        ),
-                    ),
-                    STRINGTYPE,
-                )
+                func.ST_AsMVTGeom(
+                    func.ST_Transform(PublishedRecord.geom, 3857),
+                    func.ST_TileEnvelope(tile_path.z, tile_path.x, tile_path.y),
+                ),
+                PublishedRecord.pharos_id,
+                PublishedProject.name.label("project_name"),
             )
+            .select_from(PublishedRecord)
+            .join(PublishedDataset)
+            .join(PublishedProject)
+            .cte("mvt_geom")
         )
 
-        if data is None:
-            return format_response(404, {"error": "Not found"})
+        query = select(
+            func.ST_AsMVT(mvt_geom.table_valued(), layer_name),
+        ).select_from(mvt_geom)
 
-        return format_response(200, data, preformatted=True)
+        memory = session.scalar(query)
+
+        if not memory:
+            tile_bytes = bytes()
+        else:
+            tile_bytes = bytes(memory)
+
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Access-Control-Allow-Origin": CORS_ALLOW,
+                "Content-Type": "application/octet-stream",
+            },
+            "body": base64.b64encode(tile_bytes).decode('utf-8'),
+            'isBase64Encoded': True,
+        }
